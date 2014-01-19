8a732095f453db998f7427a2556311c67a46149a
Dylan Miller <dmillerw@gmail.com>
2014-01-19 15:03:22 -0800

    Filter now takes time to process grinding

--- src/main/java/dmillerw/industrialization/block/BlockFilter.java
+++ src/main/java/dmillerw/industrialization/block/BlockFilter.java
@@ -1,20 +1,10 @@
 package dmillerw.industrialization.block;
 
-import dmillerw.industrialization.Industrialization;
 import dmillerw.industrialization.block.tile.TileCore;
 import dmillerw.industrialization.block.tile.TileFilter;
-import dmillerw.industrialization.core.handler.GuiHandler;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Icon;
-import net.minecraft.world.World;
-
-import java.util.List;
 
 /**
  * Created by Dylan Miller on 1/1/14
@@ -26,20 +16,6 @@ public class BlockFilter extends BlockCore {
 
         setHardness(2F);
         setResistance(2F);
-    }
-
-    @Override
-    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float fx, float fy, float fz) {
-        if (!world.isRemote) {
-            TileEntity tile = world.getBlockTileEntity(x, y, z);
-
-            if (tile != null && tile instanceof TileFilter) {
-                player.openGui(Industrialization.instance, GuiHandler.GUI_FILTER, world, x, y, z);
-                return true;
-            }
-        }
-
-        return false;
     }
 
     @Override
--- src/main/java/dmillerw/industrialization/block/tile/TileFilter.java
+++ src/main/java/dmillerw/industrialization/block/tile/TileFilter.java
@@ -1,40 +1,98 @@
 package dmillerw.industrialization.block.tile;
 
+import dmillerw.industrialization.block.BlockHandler;
 import dmillerw.industrialization.recipe.FilterManager;
 import dmillerw.industrialization.recipe.FilterRecipe;
+import dmillerw.industrialization.util.UtilStack;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.Vec3;
 import net.minecraftforge.common.ForgeDirection;
 
 import java.util.List;
+import java.util.Random;
+import java.util.Stack;
 
 /**
  * Created by Dylan Miller on 1/18/14
  */
 public class TileFilter extends TileCore {
 
+    public static int processingTime = 80;
+
+    private Stack<ItemStack> processingQueue = new Stack<ItemStack>();
+
+    private int currentProcessingTime = processingTime;
+
     private boolean waterFlowing = false;
 
     @Override
     public void updateEntity() {
         if (!worldObj.isRemote && waterFlowing) {
-            List<EntityItem> nearbyItems = worldObj.getEntitiesWithinAABB(EntityItem.class, AxisAlignedBB.getBoundingBox(xCoord, yCoord + 1, zCoord, xCoord + 1, yCoord + 1.5, zCoord + 1));
+            // Handle insertion of new items
+            if (worldObj.getTotalWorldTime() % 5 == 0) {
+                List<EntityItem> nearbyItems = worldObj.getEntitiesWithinAABB(EntityItem.class, AxisAlignedBB.getBoundingBox(xCoord, yCoord + 1, zCoord, xCoord + 1, yCoord + 1.5, zCoord + 1));
 
-            if (nearbyItems != null && nearbyItems.size() > 0) {
-                for (EntityItem item : nearbyItems) {
-                    FilterRecipe recipe = FilterManager.INSTANCE.getRecipeFor(item.getEntityItem());
+                if (nearbyItems != null && nearbyItems.size() > 0) {
+                    for (EntityItem item : nearbyItems) {
+                        FilterRecipe recipe = FilterManager.INSTANCE.getRecipeFor(item.getEntityItem());
 
-                    if (recipe != null) {
-                        EntityItem newItem = new EntityItem(worldObj, xCoord + 0.5, yCoord - 0.5, zCoord + 0.5, recipe.getOutput());
-                        worldObj.spawnEntityInWorld(newItem);
-                        item.setDead();
+                        if (recipe != null) {
+                            for (int i=0; i<item.getEntityItem().stackSize; i++) {
+                                processingQueue.push(UtilStack.resize(item.getEntityItem(), 1));
+                            }
+                            item.setDead();
+                            currentProcessingTime = processingTime;
+                        }
                     }
                 }
             }
+
+            // Handle item processing
+            if (processingQueue.size() > 0) {
+                if (currentProcessingTime == 0) { // If done, pop from stack and produce result
+                    ItemStack item = processingQueue.pop();
+                    FilterRecipe recipe = FilterManager.INSTANCE.getRecipeFor(item);
+                    EntityItem entity = new EntityItem(worldObj, xCoord + 0.5, yCoord - 0.5, zCoord + 0.5, recipe.getOutput());
+                    worldObj.spawnEntityInWorld(entity);
+
+                    // If still has items, restart process
+                    if (processingQueue.size() > 0) {
+                        currentProcessingTime = processingTime;
+                    }
+                }
+
+                if (currentProcessingTime > 0) {
+                    currentProcessingTime--;
+                    if (worldObj.getTotalWorldTime() % 4 == 0) { // Add particles four times a second
+                        ItemStack dust = FilterManager.INSTANCE.getRecipeFor(processingQueue.peek()).getOutput();
+                        worldObj.addBlockEvent(xCoord, yCoord, zCoord, BlockHandler.blockFilterID, dust.itemID, dust.getItemDamage());
+                    }
+                }
+            } else {
+                currentProcessingTime = processingTime;
+            }
         }
+    }
+
+    @Override
+    public boolean receiveClientEvent(int id, int param) {
+        Random rand = new Random();
+        for (int i=0; i<10; i++) {
+            Vec3 vec3 = this.worldObj.getWorldVec3Pool().getVecFromPool(((double)rand.nextFloat() - 0.5D) * 0.1D, Math.random() * 0.1D + 0.1D, 0.0D);
+            vec3.rotateAroundY(-rand.nextInt(360) * (float)Math.PI / 180.0F);
+
+            double motionX = vec3.xCoord + 0.5F * (rand.nextFloat() * (rand.nextBoolean() ? -1.0F : 1.0F));
+            double motionZ = vec3.xCoord + 0.5F * (rand.nextFloat() * (rand.nextBoolean() ? -1.0F : 1.0F));
+
+            this.worldObj.spawnParticle("iconcrack_" + id + "_" + param, xCoord + 0.5 + motionX, yCoord + 0.5, zCoord + 0.5 + motionZ, vec3.xCoord, vec3.yCoord, vec3.zCoord);
+        }
+        return true;
     }
 
     @Override
@@ -43,9 +101,17 @@ public class TileFilter extends TileCore {
     }
 
     @Override
+    public void onBlockBreak() {
+        for (ItemStack stack : processingQueue) {
+            UtilStack.dropStack(this, stack);
+        }
+    }
+
+    @Override
     public void onNeighborBlockUpdate() {
         if (!waterFlowing) {
-            if (worldObj.getBlockId(xCoord, yCoord + 1, zCoord) == Block.waterMoving.blockID) {
+            int idAbove = worldObj.getBlockId(xCoord, yCoord + 1, zCoord);
+            if (idAbove == Block.waterStill.blockID || idAbove == Block.waterMoving.blockID) {
                 waterFlowing = true;
 
                 if (worldObj.isAirBlock(xCoord, yCoord - 1, zCoord)) {
@@ -53,7 +119,8 @@ public class TileFilter extends TileCore {
                 }
             }
         } else {
-            if (worldObj.getBlockId(xCoord, yCoord + 1, zCoord) != Block.waterMoving.blockID) {
+            int idAbove = worldObj.getBlockId(xCoord, yCoord + 1, zCoord);
+            if (idAbove != Block.waterStill.blockID && idAbove != Block.waterMoving.blockID) {
                 waterFlowing = false;
 
                 if (worldObj.getBlockMaterial(xCoord, yCoord - 1, zCoord) == Material.water) {
@@ -65,12 +132,32 @@ public class TileFilter extends TileCore {
 
     @Override
     public void writeCustomNBT(NBTTagCompound nbt) {
+        if (processingQueue.size() > 0) {
+            NBTTagList queue = new NBTTagList();
+            for (ItemStack stack : processingQueue) {
+                if (stack != null) {
+                    NBTTagCompound itemTag = new NBTTagCompound();
+                    stack.writeToNBT(itemTag);
+                    queue.appendTag(itemTag);
+                }
+            }
+            nbt.setTag("queue", queue);
+        }
 
+        nbt.setInteger("processingTime", currentProcessingTime);
     }
 
     @Override
     public void readCustomNBT(NBTTagCompound nbt) {
+        if (nbt.hasKey("queue")) {
+            NBTTagList queue = nbt.getTagList("queue");
+            for (int i=0; i<queue.tagCount(); i++) {
+                NBTTagCompound itemTag = (NBTTagCompound) queue.tagAt(i);
+                processingQueue.push(ItemStack.loadItemStackFromNBT(itemTag));
+            }
+        }
 
+        nbt.setInteger("processingTime", currentProcessingTime);
     }
 
     private ForgeDirection flowDirectionFromDouble(double flow) {
